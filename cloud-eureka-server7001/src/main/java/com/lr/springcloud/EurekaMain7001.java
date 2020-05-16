2c8509b296992a666f1041f5d7b709fd3bbc02ec
soymilk9521 <soymilk9521@gmail.com>
2020-05-16 21:23:37 +0900

    项目结构调整，添加公共模块，配置Eureka服务注册中心

---
+++ cloud-eureka-server7001/src/main/java/com/lr/springcloud/EurekaMain7001.java
@@ -0,0 +1,21 @@
+package com.lr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author KR
+ * @since 2020/05/16 20:10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
