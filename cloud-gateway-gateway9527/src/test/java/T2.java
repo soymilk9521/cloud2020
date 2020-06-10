6556eb23e8a03f2e93b93a9df7c60f7a840cfd3b
soymilk9521 <soymilk9521@gmail.com>
2020-06-10 20:45:55 +0900

    SpringCloud Gateway断言和过滤器

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,16 @@
+import java.time.ZonedDateTime;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LR
+ * @since 2020/06/09 7:54
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
