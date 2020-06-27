c6c34856bcb0d09031424b635e7997a533095282
soymilk9521 <soymilk9521@gmail.com>
2020-06-27 17:20:28 +0900

    springCloud Alibaba Nacos服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/lr/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.lr.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LR
+ * @since 2020/06/27 15:07
+ */
+@RestController
+@RefreshScope // 支持nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
