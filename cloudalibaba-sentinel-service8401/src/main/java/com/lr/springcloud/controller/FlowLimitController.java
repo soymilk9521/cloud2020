2d6315eb15a5618e91d6860e0841dd9ad34fc273
soymilk9521 <soymilk9521@gmail.com>
2020-06-30 23:09:38 +0900

    springCloud Alibaba Sentinel服务降级（平均响应时间，异常比例，异常数）

--- cloudalibaba-sentinel-service8401/src/main/java/com/lr/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lr/springcloud/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.lr.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +13,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020/06/29 8:32
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+//        try {
+//            Thread.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return "---------testA";
     }
 
     @GetMapping("/testB")
     public String testB(){
         return "---------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试RT");
+//        log.info("testD 测试异常比例");
+        log.info("testD 测试异常数");
+        int age = 10/0;
+        return "---------testD";
     }
 }
