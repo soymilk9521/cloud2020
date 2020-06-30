7c2fe7c65ba1c714d5af15847f9e0ee7010c0677
soymilk9521 <soymilk9521@gmail.com>
2020-07-01 08:53:01 +0900

    springCloud Alibaba Sentinel服务降级（热点key限流，系统规则限流）

--- cloudalibaba-sentinel-service8401/src/main/java/com/lr/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lr/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.lr.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -43,5 +46,16 @@ public class FlowLimitController {
         log.info("testD 测试异常数");
         int age = 10/0;
         return "---------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false)String p1,
+                             @RequestParam(value = "p2", required = false)String p2){
+        return "----------testHotKey";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException e) {
+        return "----------testHotKeyo(╥﹏╥)o";
     }
 }
